8a0ab6e4d6a2fc971e336702d051985cefb727df
Jordan Powell <powelj87@gmail.com>
2017-12-20 20:37:08 -0500

    first commit

---
+++ emacs.el
@@ -0,0 +1,366 @@
+
+(setq user-full-name "Jordan Powell")
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(blink-cursor-mode 0)
+
+(show-paren-mode 1)
+
+(setq-default indent-tabs-mode 1)
+(setq tab-width 2)
+
+(setq sentence-end-double-space nil)
+
+(global-visual-line-mode)
+  :diminish 'visual-line-mode
+
+(setq ring-bell-function 'ignore)
+
+(defun comment-region-timestamp ()
+  (interactive)
+  (setq timestamp (format-time-string "(%d-%b-%y)"))
+  (insert (format ";; jdp %s: " timestamp)))
+
+(set-default-font "Nimbus Mono L Bold 14")
+
+(use-package spacemacs-theme
+  :ensure t
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t))
+
+(defun switch-theme (theme)
+  "Disables any currently active themes and loads THEME."
+  ;; This interactive call is taken from `load-theme'
+  (interactive
+   (list
+    (intern (completing-read "Load custom theme: "
+                             (mapc 'symbol-name
+                                   (custom-available-themes))))))
+  (let ((enabled-themes custom-enabled-themes))
+    (mapc #'disable-theme custom-enabled-themes)
+    (load-theme theme t)))
+
+(bind-key "C-<f12>" 'switch-theme)
+
+(column-number-mode 1)
+(global-linum-mode 1)
+
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(tooltip-mode 0)
+
+(setq inhibit-startup-message 1)
+(setq initial-scratch-message "")
+
+(use-package ag
+  :ensure t
+  :commands ag)
+
+(use-package ace-jump-mode
+  :ensure t 
+  :defer 5
+  :diminish ace-jump-mode
+  :commands ace-jump-mode
+  :bind ("C-S-s" . ace-jump-mode))
+
+(use-package ace-window
+  :ensure t
+  :config
+  (setq aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
+  (ace-window-display-mode)
+  :bind ("s-o" . ace-window))
+
+(use-package c-eldoc
+  :commands c-turn-on-eldoc-mode
+  :ensure t
+  :init (add-hook 'c-mode-hook #'c-turn-on-eldoc-mode))
+
+(use-package cc-mode
+  :mode (("\\.h\\(h?\\|xx\\|pp\\)\\'" . c++-mode)
+         ("\\.m\\'" . c-mode)
+         ("\\.mm\\'" . c++-mode))
+  :bind (:map c++-mode-map
+              ("<" . self-insert-command)
+              (">" . self-insert-command))
+  :bind (:map c-mode-base-map
+              ("#" . self-insert-command)
+              ("{" . self-insert-command)
+              ("}" . self-insert-command)
+              ("/" . self-insert-command)
+              ("*" . self-insert-command)
+              (";" . self-insert-command)
+              ("," . self-insert-command)
+              (":" . self-insert-command)
+              ("(" . self-insert-command)
+              (")" . self-insert-command)
+              ("<return>" . newline-and-indent)
+              ("M-q" . c-fill-paragraph)
+              ("M-j"))
+  :preface
+  (defun my-c-mode-common-hook ()
+    (set (make-local-variable 'parens-require-spaces) nil)
+
+    (let ((bufname (buffer-file-name)))
+      (when bufname
+        (cond
+         ((string-match "/ledger/" bufname)
+          (c-set-style "ledger"))
+         ((string-match "/edg/" bufname)
+          (c-set-style "edg"))
+         (t
+          (c-set-style "clang")))))
+
+    (font-lock-add-keywords
+     'c++-mode '(("\\<\\(assert\\|DEBUG\\)(" 1 font-lock-warning-face t))))
+
+  :hook (c-mode-common . my-c-mode-common-hook)
+  :config
+  (add-to-list
+   'c-style-alist
+   '("edg"
+     (indent-tabs-mode . nil)
+     (c-basic-offset . 2)
+     (c-comment-only-line-offset . (0 . 0))
+     (c-hanging-braces-alist
+      . ((substatement-open before after)
+         (arglist-cont-nonempty)))
+     (c-offsets-alist
+      . ((statement-block-intro . +)
+         (knr-argdecl-intro . 5)
+         (substatement-open . 0)
+         (substatement-label . 0)
+         (label . 0)
+         (case-label . +)
+         (statement-case-open . 0)
+         (statement-cont . +)
+         (arglist-intro . +)
+         (arglist-close . +)
+         (inline-open . 0)
+         (brace-list-open . 0)
+         (topmost-intro-cont
+          . (first c-lineup-topmost-intro-cont
+                   c-lineup-gnu-DEFUN-intro-cont))))
+     (c-special-indent-hook . c-gnu-impose-minimum)
+     (c-block-comment-prefix . "")))
+
+  (add-to-list
+   'c-style-alist
+   '("ledger"
+     (indent-tabs-mode . nil)
+     (c-basic-offset . 2)
+     (c-comment-only-line-offset . (0 . 0))
+     (c-hanging-braces-alist
+      . ((substatement-open before after)
+         (arglist-cont-nonempty)))
+     (c-offsets-alist
+      . ((statement-block-intro . +)
+         (knr-argdecl-intro . 5)
+         (substatement-open . 0)
+         (substatement-label . 0)
+         (label . 0)
+         (case-label . 0)
+         (statement-case-open . 0)
+         (statement-cont . +)
+         (arglist-intro . +)
+         (arglist-close . +)
+         (inline-open . 0)
+         (brace-list-open . 0)
+         (topmost-intro-cont
+          . (first c-lineup-topmost-intro-cont
+                   c-lineup-gnu-DEFUN-intro-cont))))
+     (c-special-indent-hook . c-gnu-impose-minimum)
+     (c-block-comment-prefix . "")))
+
+  (add-to-list
+   'c-style-alist
+   '("clang"
+     (indent-tabs-mode . nil)
+     (c-basic-offset . 2)
+     (c-comment-only-line-offset . (0 . 0))
+     (c-hanging-braces-alist
+      . ((substatement-open before after)
+         (arglist-cont-nonempty)))
+     (c-offsets-alist
+      . ((statement-block-intro . +)
+         (knr-argdecl-intro . 5)
+         (substatement-open . 0)
+         (substatement-label . 0)
+         (label . 0)
+         (case-label . 0)
+         (statement-case-open . 0)
+         (statement-cont . +)
+         (arglist-intro . +)
+         (arglist-close . +)
+         (inline-open . 0)
+         (brace-list-open . 0)
+         (topmost-intro-cont
+          . (first c-lineup-topmost-intro-cont
+                   c-lineup-gnu-DEFUN-intro-cont))))
+     (c-special-indent-hook . c-gnu-impose-minimum)
+     (c-block-comment-prefix . ""))))
+
+(use-package clojure-mode
+  :defer t
+  :ensure t)
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package flycheck
+  :ensure t
+  :defer 10
+  :config (setq flycheck-html-tidy-executable "tidy5"))
+
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :init (progn
+          (require 'helm-config)
+          (use-package helm-projectile
+            :ensure t
+            :commands helm-projectile
+            :bind ("C-c p h" . helm-projectile))
+          (use-package helm-ag :defer 10  :ensure t)
+          (setq helm-locate-command "mdfind -interpret -name %s %s"
+                helm-ff-newfile-prompt-p nil
+                helm-M-x-fuzzy-match t)
+          (helm-mode)
+          (use-package helm-swoop
+            :ensure t
+            :bind ("H-w" . helm-swoop)))
+  :bind (("C-c h" . helm-command-prefix)
+         ("C-x b" . helm-mini)
+         ("C-`" . helm-resume)
+         ("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files)))
+
+(use-package macrostep
+  :ensure t
+  :bind ("C-h e" . macrostep-expand)
+        ("C-h c" . macrostep-collapse))
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(require 'use-package)
+(use-package neotree
+  :ensure t
+  :commands neotree-mode
+  :diminish neotree-mode
+  :bind (("<f2>" . neotree-toggle))
+  :config
+  (setq neo-theme 'icons)
+  (evil-set-initial-state 'neotree-mode 'normal)
+  (evil-define-key 'normal neotree-mode-map
+    (kbd "RET") 'neotree-enter
+    (kbd "h") 'neotree-hidden-file-toggle
+    (kbd "R") 'neotree-refresh
+    ))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package org
+  :ensure t
+  :ensure org-plus-contrib)
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(bind-key "C-c l" 'org-store-link)
+(bind-key "C-c c" 'org-capture)
+(bind-key "C-c a" 'org-agenda)
+
+(setq org-agenda-files
+      (delq nil
+            (mapcar (lambda (x) (and (file-exists-p x) x))
+                    '("~/Dropbox/Agenda"))))
+
+(bind-key "C-c c" 'org-capture)
+(setq org-default-notes-file "~/Dropbox/Notes/notes.org")
+
+(setq org-use-speed-commands t)
+
+(setq org-image-actual-width 550)
+
+(setq org-highlight-latex-and-related '(latex script entities))
+
+(setq org-tags-column 45)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (C . t)
+   (calc . t)
+   (latex . t)
+   (java . t)
+   (ruby . t)
+   (lisp . t)
+   (scheme . t)
+   (shell . t)
+   (sqlite . t)
+   (js . t)))
+
+(defun my-org-confirm-babel-evaluate (lang body)
+  "Do not confirm evaluation for these languages."
+  (not (or (string= lang "C")
+           (string= lang "java")
+           (string= lang "python")
+           (string= lang "emacs-lisp")
+           (string= lang "sqlite"))))
+(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
+
+(setq org-src-fontify-natively t
+      org-src-window-setup 'current-window
+      org-src-strip-leading-and-trailing-blank-lines t
+      org-src-preserve-indentation t
+      org-src-tab-acts-natively t)
+
+(setq org-latex-pdf-process (list "latexmk -pdf %f"))
+
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :commands (projectile-mode projectile-switch-project)
+  :bind (("C-c p p" . projectile-switch-project)
+         ("C-c p s s" . projectile-ag))
+  :config
+  (projectile-global-mode t)
+  (setq projectile-enable-caching t)
+  (setq projectile-switch-project-action 'projectile-dired))
+
+(use-package undo-tree
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :config
+  (yas-global-mode))
+
+(setq display-time-default-load-average nil)
+
+(setq battery-mode-line-format "[%b%p%% %t]")
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
